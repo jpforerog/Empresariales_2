1a471017c4d0ea76f2574e0a905fa3e515c6672a
Juan Pablo Forero <juan.forero1@estudiantesunibague.edu.co>
2025-03-31 14:55:35 -0500

    Put completo

--- src/main/java/com/ProyectoEmpresariales/Arma/controller/ArmaController.java
+++ src/main/java/com/ProyectoEmpresariales/Arma/controller/ArmaController.java
@@ -8,15 +8,19 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/Arma")
 
 
@@ -54,14 +58,14 @@ public class ArmaController {
     }
     @PostMapping(value = "/")
     //Revisar que no entren jsons nulos
-    public ResponseEntity<String> añadirRifle(@RequestBody JsonNode jsonNode){
+    public ResponseEntity añadirRifle(@RequestBody JsonNode jsonNode){
         objectMapper.registerModule(new JavaTimeModule());
         if(!jsonNode.get("nombre").asText().isEmpty()){
             try {
                 Rifle rifle = objectMapper.treeToValue(jsonNode, Rifle.class);
                 Arma rifle1 = (Arma) rifle;
                 servicioArma.añadirArma(rifle1);
-                return ResponseEntity.ok("Arma agregada correctamente");
+                return new ResponseEntity<>(rifle1,HttpStatus.ACCEPTED);
             }catch (Exception e){
                 e.printStackTrace();
                 return ResponseEntity.badRequest().body("Error: Datos del arma incompletos");
@@ -70,6 +74,7 @@ public class ArmaController {
             return ResponseEntity.badRequest().body("Arma sin nombre");
 
     }
+    //Solo pruebas
     @GetMapping(value = "")
     public void test(){
         try {
@@ -79,6 +84,7 @@ public class ArmaController {
             throw new RuntimeException(e);
         }
     }
+
     @DeleteMapping(value = "/")
     public List<Arma> eliminarArma(@RequestBody JsonNode jsonNode){
 
@@ -88,6 +94,7 @@ public class ArmaController {
         if (tipo.equals("Rifle")){
             tipo="class com.ProyectoEmpresariales.Arma.model.Rifle";
         }
+
         for (Arma arma: servicioArma.getArmas()){
 
             if(arma.getNombre().equals(nombre)&&String.valueOf(arma.getClass()).equals(tipo)){
@@ -100,34 +107,55 @@ public class ArmaController {
         }return servicioArma.getArmas();
     }
     @PutMapping(value = "/")
-    public Arma actualizarArma(@RequestBody JsonNode jsonNode){
-        Arma arma1 = servicioArma.getArmas().get(0);
-        String nombre = jsonNode.get("nombre").asText();
-        String tipo = jsonNode.get("tipo").asText();
-        if (tipo.equals("Rifle")){
-            tipo="class com.ProyectoEmpresariales.Arma.model.Rifle";
+    public ResponseEntity actualizarArma(@RequestBody JsonNode jsonNode){
+        String nombre ="";
+
+        if(!jsonNode.get("nombre").asText().isEmpty()) {
+
+            nombre = jsonNode.get("nombre").asText();
+        }else{
+            System.out.println("nulo");
+            ResponseEntity.badRequest().body("Nombre de arma nulo");
         }
+        String tipo = "";
+        if (jsonNode.get("tipo").asText().equals("Rifle")){
+
+            tipo="class com.ProyectoEmpresariales.Arma.model.Rifle";
+        }else{
+            ResponseEntity.badRequest().body("Tipo de arma no encontrado");
+        }
+
         for (Arma arma: servicioArma.getArmas()){
 
-            if(arma.getNombre().equals(nombre) && String.valueOf(arma.getClass()).equals(tipo)){
-                System.out.println(servicioArma.getArmas());
+            if(arma.getNombre().equals(nombre) && String.valueOf(arma.getClass()).equals(tipo) && !jsonNode.get("nombre").asText().isEmpty()){
+
                 ObjectNode objectNode = (ObjectNode) jsonNode;
                 objectNode.remove("tipo");
+                if(jsonNode.get("nombreNuevo").asText().isEmpty()){
+                    objectNode.remove("nombreNuevo");
+                }else {
+                    String n = jsonNode.get("nombreNuevo").asText();
+                    objectNode.put("nombre",n);
+                    objectNode.remove("nombreNuevo");
+                }
                 JsonNode json = (JsonNode) objectNode;
                 try {
                     Rifle armaAct = objectMapper.treeToValue(json, Rifle.class);
                     Arma armaAct1 = (Arma) armaAct;
                     servicioArma.actualizarArma(arma,armaAct1);
+                    return new ResponseEntity<>(armaAct1,HttpStatus.ACCEPTED);
                 } catch (JsonProcessingException e) {
                     throw new RuntimeException(e);
                 }
 
 
-                System.out.println(servicioArma.getArmas());
-                return arma;
+
+
 
             }
-        }return null;
+        }
+
+        return new ResponseEntity<>("Arma no encontrada", HttpStatus.BAD_REQUEST);
     }
     /*@DeleteMapping(value = "/")
     //no es igual la entrada
